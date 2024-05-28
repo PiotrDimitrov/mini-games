388a677c59fbb3df59ac86c9f0d5a426a01d3596
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-05-28 14:29:31 +0300

    initial mini game menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "sudoku.h"
 #include "game.h"
+#include <windows.h>
 
 int main() {
-game newGame;   //create game instance to start game
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    int k;
+    for(k = 1; k <= 256; k++)
+    {
+        // pick the colorattribute k you want
+        SetConsoleTextAttribute(hConsole, k);
+        std::cout << k << " I want to be nice today!" << std::endl;
+    }
+
+//game newGame;   //create game instance to start game
 }
