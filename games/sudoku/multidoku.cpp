1bc93603068cc548016606ee2816fce7a1edbc7f
PiotrDimitrov <piotr.dimitrov2019@gmail.com>
2024-06-02 16:56:10 +0300

    doubledoku and tripledoku

---
+++ games/sudoku/multidoku.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 01.06.2024.
+//
+
+#include "multidoku.h"
---
+++ games/sudoku/multidoku.h
@@ -0,0 +1,16 @@
+//
+// Created by User on 01.06.2024.
+//
+
+#ifndef SUDOKU_MULTIDOKU_H
+#define SUDOKU_MULTIDOKU_H
+
+
+
+class multidoku {
+
+};
+
+
+
+#endif //SUDOKU_MULTIDOKU_H
